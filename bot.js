66c64e7efc25591c77782b99a106e67e1b7e9e33
Glitch (test-twitch-bot) <none>
2019-12-08 13:06:01 +0000

    🌯😚 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,40 +1,43 @@
-const tmi = require('tmi.js');
+const tmi = require("tmi.js");
 
 const opts = {
   identity: {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN
   },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
+  channels: [process.env.CHANNEL_NAME]
 };
 
-const channelName = 'ponchi_one'
+var channelName = process.env.CHANNEL_NAME;
 const client = new tmi.client(opts);
+client.on("message", onMessageHandler);
+client.on("connected", onConnectedHandler);
+//client.on("raided", onRaided);
 
-client.on('connected', onConnectedHandler);
 client.connect();
-
-client.on('chat', (channel, user, message, self) => {  
-  const commandName = message.trim();
-  if (self) {return;}
-  if (user['username'] === client.getUsername()){
+function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    // Ignore messages from the bot
     return;
   }
-  if (commandName.includes('что за сервер') || 
-  commandName.includes('какой сервер') || commandName.includes('!Сервер') || commandName.includes('!сервер') || 
-  commandName.includes('что за сервак') || commandName.incudes('какой сервак') || commandName.includes('!server') || commandName.includes('!Server')) {
-    client.say(channelName, `@${user['display-name']} L2 Essence, Server: White, ruoff`);
-    console.log(`* Executed ${commandName} command`);
-  }
-});
 
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+  if (context["custom-reward-id"] == "5f6a55b8-43a0-4794-91f6-951ac97c2e1b") {
+    // defines reward id
+    client.timeout(channelName, context["username"], 600)
+      .catch((err) => {});
+    console.log(`* Command executed`);
+  }
 }
 
-client.on('disconnected', (reason) => {
-  console.log('Disconnected from server ' + reason);
-  client.connect();
-});
+// function onRaided(channel, username, viewers) {
+//   let start = Date.now();
+//   client.followersonlyoff(channelName);
+
+//   if (Date.now() - start == 600000) {
+//     client.followersonly(channelName, 10);
+//   }
+// }
+
+function onConnectedHandler(addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
--- rubbish-bin.js
+++ rubbish-bin.js
@@ -10,31 +10,29 @@ const opts = {
   ]
 };
 
-const channelName = 'ponchi_one'
+const channelName = process.env.CHANNEL_NAME;
 const client = new tmi.client(opts);
 
 client.on('connected', onConnectedHandler);
 client.connect();
 
-client.on('chat', (channel, user, message, self) => {  
+client.on('chat', (channel, user, message, self) => {
   const commandName = message.trim();
-  if (self) {return;}
-  if (user['username'] === client.getUsername()){
-    return;
-  }
+  // if (self) {return;}
+  // if (user['username'] === client.getUsername()){
+  //   return;
+  // }
   if (commandName.includes('что за сервер') || 
-  commandName.includes('какой сервер') || commandName.includes('!Сервер') || commandName.includes('!сервер') || 
-  commandName.includes('что за сервак') || commandName.incudes('какой сервак') || commandName.includes('!server') || commandName.includes('!Server')) {
+  commandName.includes('какой сервер') || commandName === '!Сервер' || commandName === '!сервер' || 
+  commandName.includes('что за сервак') || commandName.includes('какой сервак') || commandName.includes('!server') || 
+  commandName.includes('!Server') || commandName.includes('какой серв') || commandName.includes('что за серв') || commandName.includes('что это за сервер') ||
+  commandName.includes('Что за сервак') || commandName.includes('Что за сервер') || commandName.includes('Что это за сервер') ||
+  commandName.includes('Что это за сервак') || commandName.includes('Какой сервак') || commandName.includes('Какой сервер')) {
     client.say(channelName, `@${user['display-name']} L2 Essence, Server: White, ruoff`);
     console.log(`* Executed ${commandName} command`);
   }
 });
-
+// (sent by bot) :robot:
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
-
-client.on('disconnected', (reason) => {
-  console.log('Disconnected from server ' + reason);
-  client.connect();
-});
